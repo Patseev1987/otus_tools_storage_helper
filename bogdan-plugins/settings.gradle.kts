5fcd55885e34bc897333ad5030275cff6aea0b86
Bogdan Patseev <patseev1987@gmail.com>
2024-10-06 17:29:04 +0300

    add plugins

---
+++ bogdan-plugins/settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "tools-helper-backend-build"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
